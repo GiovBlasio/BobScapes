79b37f664efd048e685ce252246460871fdae9c9
Giovanni Blasio <blasio.giovanni@hotmail.com>
2023-08-02 11:29:18 +0200

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  // locator.registerLazySingleton(() => AnalyticsService());
+  
   runApp(
     MultiProvider(
       providers: [
